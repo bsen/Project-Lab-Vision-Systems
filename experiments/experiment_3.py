ac5e882d78b26ec5eb2e4ff76ee6ba5ed97e8cc7
Ben Rank <ben.rank@posteo.de>
2021-09-10 09:19:15 +0200

    Change hyperparameter config a bit and add CustomStopper to exp 3

--- experiments/experiment_3.py
+++ experiments/experiment_3.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from ray import tune
 import torch_warmup_lr
 import os
+from collections import defaultdict, deque
 
 import sys
 sys.path.insert(0, './')
@@ -29,11 +31,10 @@ pretrained_path = '/home/user/brank/project/Project-Lab-Vision-Systems/saved_mod
 
 # the configuration for the hyperparameter search
 config = {
-        'lr': tune.loguniform(3e-6, 5e-3),
-        'batch_size': tune.choice([4,5]),
-        'step_lr': tune.quniform(35, 90, q=1.0),
+        'lr': tune.loguniform(3e-5, 5e-3),
+        'step_lr': tune.quniform(45, 90, q=1.0),
         'num_warmup': tune.quniform(4, 8, q=1),
-        'dropout_p': tune.uniform(0.0, 0.5)
+        'dropout_p': tune.uniform(0.0, 0.3)
         }
 
 # the function which trains the model and reports checkpoints given
@@ -41,7 +42,7 @@ config = {
 def train(config, checkpoint_dir=None):
 
     kitti_loader = torch.utils.data.DataLoader(dataset=kitti_train,
-                                               batch_size=config['batch_size'],
+                                               batch_size=5,
                                                shuffle=True)
     channel_fe = layers['feat']
     channel_cp = layers['cost']
@@ -63,14 +64,17 @@ def train(config, checkpoint_dir=None):
                                          warmup_strategy='cos')
 
     if checkpoint_dir:
-        model_state, optimizer_state = torch.load(
+        states = torch.load(
             os.path.join(checkpoint_dir, "checkpoint"))
+        model_state, optimizer_state, scheduler_state = states
+        
+        scheduler_state.load_state_dict(scheduler_state)
         model.load_state_dict(model_state)
         optimizer.load_state_dict(optimizer_state)
     
     # training
     training.train_model(model=model, optimizer=optimizer, scheduler=scheduler,
-            train_loader=kitti_loader, num_epochs=180, log_dir=None,
+            train_loader=kitti_loader, num_epochs=100, log_dir=None,
             valid_loader=kitti_val_loader, savefile=None,
             mes_time=False, use_amp=True, show_graph=False,
             use_tune=True, warmup_lr=True)
@@ -79,8 +83,92 @@ def train(config, checkpoint_dir=None):
 # set custom random seed
 set_random_seed(21)
 
+# build a custom stopper which early stops single trials
+
+class CustomStopper(tune.Stopper):
+    """
+    Some parts of the code of this class are copied from the TrialPlateauStopper
+    code from raytune.
+    """
+    
+    def __init__(self, std, grace_period, num_results):
+        self._std = std
+        self._grace_period = grace_period
+        self._num_results = num_results
+        
+        self._iter = defaultdict(lambda: 0)
+        self._trial_results = defaultdict(
+                    lambda: deque(maxlen=self._num_results))
+        self._errors = defaultdict(
+                    lambda: deque(maxlen=self._num_results))
+
+    def __call__(self, trial_id, result):
+        """
+        Gets as input the current trial and stops it when the function was called more
+        than grace_period amount of times for this trial_id and
+        one of the following criteria are met:
+        - the standard deviation on the last num_results is lower than std (for this trial_id)
+        - the function was called more than 14 times (we are in iteration >=40) and
+            the error is still higher than 0.2
+        - the function was called more than 20 times (we are in iteration >=58) and
+            the error is still higher than 0.1
+        - the function was called more than 33 times and the mean error of the
+            last num_results results is higher than
+            0.085
+        - the loss is nan or infinity (this happend to me in the past
+                                       due to using automatic mixed precision)
+        """
+        loss = result.get('loss')
+        err = result.get('err')
+        
+        self._trial_results[trial_id].append(loss)
+        self._errors[trial_id].append(err)
+        self._iter[trial_id] += 1
+        
+        # If still in grace period, do not stop yet
+        if self._iter[trial_id] < self._grace_period:
+            return False
+
+        if (self._iter[trial_id] >= 14) and (err >= 0.2):
+            return True
+        
+        if (self._iter[trial_id] >= 20) and (err >= 0.1):
+            return True
+        
+        if np.isnan(loss):
+            print('The loss is nan')
+            return True
+        
+        if np.isinf(loss):
+            print('The loss is inf: loss =', loss)
+            return True
+        
+        try:
+            if (self._iter[trial_id] >= 30) and (np.mean(self._errors[trial_id]) >= 0.085):
+                return True
+        except Exception:
+            pass
+        
+        # If not enough results yet, do not stop yet
+        if len(self._trial_results[trial_id]) < self._num_results:
+            return False
+        
+        # Calculate stdev of last `num_results` results
+        try:
+            current_std = np.std(self._trial_results[trial_id])
+        except Exception:
+            current_std = float("inf")
+
+        # If stdev is lower than threshold, stop early.
+        return current_std < self._std
+
+
+    def stop_all(self):
+        return False
+
 # use early stopping
-trial_stop = tune.stopper.TrialPlateauStopper(metric='loss', std=0.005, num_results=4, grace_period=6)
+trial_stop = CustomStopper(std=0.015, grace_period=4, num_results=4)
+
 
 # do hyperparameter search
 result = result = tune.run(
